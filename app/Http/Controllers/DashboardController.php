4a92e2cf1790355916d765e0af8cd7867c3b7af8
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2023-01-23 03:41:24 +0700

    update halaman dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Artikel\Artikel;
+use App\Models\Galeri;
 use App\Models\User;
+use App\Models\Contact\Message as ContactMessage;
+use App\Models\Struktur;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -11,9 +15,17 @@ class DashboardController extends Controller
     {
 
         $total_anggota = User::count();
-        $page_attr = ['title' => 'Dashboard'];
+        $total_artikel = Artikel::count();
+        $total_galeri = Galeri::count();
+        $total_pesan = ContactMessage::count();
+        $total_pengurus = Struktur::count();
+        $page_attr = ['title' => 'Halaman Utama'];
         return view('member.dashborard', compact(
             'total_anggota',
+            'total_artikel',
+            'total_galeri',
+            'total_pesan',
+            'total_pengurus',
             'page_attr',
         ));
     }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,23 +2,89 @@
 
 @section('content')
     <div class="page-header">
-        <h1 class="page-title">Dashboard</h1>
+        <h1 class="page-title">Halaman Utama</h1>
     </div>
 
     <!-- ROW OPEN -->
     <div class="row">
         <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3">
-            <div class="card bg-primary img-card box-primary-shadow card-main">
-                <div class="card-body">
-                    <div class="d-flex">
-                        <div class="text-white">
-                            <h2 class="mb-0 number-font">{{ $total_anggota }}</h2>
-                            <p class="text-white mb-0">Total Users </p>
+            <a href="{{ route('admin.user') }}">
+                <div class="card bg-primary img-card box-primary-shadow card-main">
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <div class="text-white">
+                                <h2 class="mb-0 number-font">{{ $total_anggota }}</h2>
+                                <p class="text-white mb-0">Jumlah Pengguna </p>
+                            </div>
+                            <div class="ms-auto"> <i class="fas fa-users text-white fs-30 me-2 mt-2"></i> </div>
                         </div>
-                        <div class="ms-auto"> <i class="fas fa-users text-white fs-30 me-2 mt-2"></i> </div>
                     </div>
                 </div>
-            </div>
+            </a>
+        </div>
+
+        <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3">
+            <a href="{{ route('admin.artikel.data') }}">
+                <div class="card bg-secondary img-card box-secondary-shadow card-main">
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <div class="text-white">
+                                <h2 class="mb-0 number-font">{{ $total_artikel }}</h2>
+                                <p class="text-white mb-0">Jumlah Artikel </p>
+                            </div>
+                            <div class="ms-auto"> <i class="fas fa-file-alt text-white fs-30 me-2 mt-2"></i> </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3">
+            <a href="{{ route('admin.galeri') }}">
+                <div class="card bg-indigo img-card box-indigo-shadow card-main">
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <div class="text-white">
+                                <h2 class="mb-0 number-font">{{ $total_galeri }}</h2>
+                                <p class="text-white mb-0">Jumlah Gelri </p>
+                            </div>
+                            <div class="ms-auto"> <i class="fas fa-images text-white fs-30 me-2 mt-2"></i> </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3">
+            <a href="{{ route('admin.kontak.message') }}">
+                <div class="card bg-info img-card box-info-shadow card-main">
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <div class="text-white">
+                                <h2 class="mb-0 number-font">{{ $total_pesan }}</h2>
+                                <p class="text-white mb-0">Jumlah Pesan Diterima </p>
+                            </div>
+                            <div class="ms-auto"> <i class="fas fa-envelope text-white fs-30 me-2 mt-2"></i> </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3">
+            <a href="{{ route('admin.struktur') }}">
+                <div class="card bg-warning img-card box-warning-shadow card-main">
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <div class="text-white">
+                                <h2 class="mb-0 number-font">{{ $total_pengurus }}</h2>
+                                <p class="text-white mb-0">Jumlah Pengurus </p>
+                            </div>
+                            <div class="ms-auto"> <i class="fas fa-network-wired text-white fs-30 me-2 mt-2"></i> </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
         </div>
 
         <!-- COL END -->
@@ -28,7 +94,8 @@
                     <div class="card-body">
                         <div class="d-flex">
                             <div class="text-white">
-                                <h3 class="mb-0 number-font">Change Password</h3>
+                                <h3 class="mb-0 number-font">Ganti Password</h3>
+                                <p class="text-white mb-0"><br></p>
                             </div>
                             <div class="ms-auto"> <i class="fas fa-key text-white fs-30 me-2 mt-2"></i> </div>
                         </div>
@@ -52,3 +119,11 @@
         }
     </style>
 @endsection
+
+{{-- 
+    - Jumlah pengguna [link]
+    - Jumlah artikel yang ada [link]
+    - Jumlah galeri [link]
+    - Jumlah Pesan Diterima [link]
+    - Jumlah Pengurus [link]
+    --}}
